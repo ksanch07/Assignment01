95a2a47559210992204c4fc55c0fe7dcdc4c0aa8
Keoni Sanchez <keoni.sanchez@gmail.com>
2025-02-13 09:43:28 -0800

    first 5 only script

---
+++ Assignment01/js/script.js
@@ -0,0 +1,5 @@
+// let someMonth, theMonth, currentMonth, summerMonth, myLibraryFunction;
+// let numericLiteral = 27, stringLiteral = 'Keoni is coding', booleanLiteral = true, nullLiteral = null;
+// let complexVariableExpression1 = (5 + 3) * 2, complexVariableExpression2LongOfName = Math.pow(2, 3) + Math.sqrt(16)
+// let strFirstName, LastName, strAddress, City, State, strZipCode, YourAge, ReferralSource, boolMayWeContactYou;
+// strFirstName = "Keoni"; LastName = "Sanchez"; strAddress = "123 Main St";
