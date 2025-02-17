870fcc9e80b45b9feffa36fdcbec3cc6e08c1ccc
Keoni Sanchez <keoni.sanchez@gmail.com>
2025-02-16 21:02:26 -0800

    final script.js

--- Assignment01/js/script.js
+++ Assignment01/js/script.js
@@ -10,5 +10,13 @@
 // console.log(typeof "Hello"); console.log(typeof 42); console.log(typeof true); console.log(typeof {}); console.log(typeof undefined);
 // alert("Hello " + "Keoni Sanchez" + ", welcome to the JavaScript class!");
 // let name = "Keoni Sanchez"; alert("Hello " + name + ", welcome to the JavaScript class!"); // name is resrved word so this is a bad idea -- let userName = "Keoni Sanchez"; alert("Hello " + userName + ", welcome to the JavaScript class!");
-let course = "JavaScript"; let name = "Keoni Sanchez"; alert("Hello " + name + ", welcome to the " + course + " class!");
-console.log("Hello Dani!");
+// let course = "JavaScript"; let name = "Keoni Sanchez"; alert("Hello " + name + ", welcome to the " + course + " class!"); // added name variable so it runs as standalone line
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!"); //use same variables above
+// name = prompt("What is your name?"); let course = "Javascript"; alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+// name = prompt("What is your name?"); course = prompt("What class are you taking?"); alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+//let x = 10; let y = 20; console.log(x + y);
+// x = 20; x += 20; console.log(x);
+// x = 20; x *= 5; console.log(x);
+// let x = 20 % 3; x /= 1; console.log(x); // not let y = 20 / 3; y /= 1; console.log(y);
+// console.log(5 > 3 && 2 < 4); // greater than, and, less than. True
+// console.log(10 === '10' || 7 <= 5); // exact type equal, or, less than or equal. False
