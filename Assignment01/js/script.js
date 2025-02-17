e7fd8916aa5260e87b152267d8b3129a78acdc8e
Keoni Sanchez <keoni.sanchez@gmail.com>
2025-02-16 20:41:56 -0800

    updated alert

--- Assignment01/js/script.js
+++ Assignment01/js/script.js
@@ -10,5 +10,5 @@
 // console.log(typeof "Hello"); console.log(typeof 42); console.log(typeof true); console.log(typeof {}); console.log(typeof undefined);
 // alert("Hello " + "Keoni Sanchez" + ", welcome to the JavaScript class!");
 // let name = "Keoni Sanchez"; alert("Hello " + name + ", welcome to the JavaScript class!"); // name is resrved word so this is a bad idea -- let userName = "Keoni Sanchez"; alert("Hello " + userName + ", welcome to the JavaScript class!");
-let course = "JavaScript"; alert("Hello " + name + ", welcome to the " + course + " class!");
+let course = "JavaScript"; let name = "Keoni Sanchez"; alert("Hello " + name + ", welcome to the " + course + " class!");
 console.log("Hello class!");
